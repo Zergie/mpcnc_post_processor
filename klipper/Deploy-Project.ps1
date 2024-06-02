3f6b5f1fdbec2c033015026a36310b66e5914001
Wolfgang Puchinger <wpuchinger@rocom.de>
2024-06-02 19:34:14 +0200

    Bugfixes

--- klipper/Deploy-Project.ps1
+++ klipper/Deploy-Project.ps1
@@ -1,50 +1,24 @@
 [CmdletBinding()]
 param()
-$klipper_url = "trident.local"
-$klipper_user = "biqu"
+$klipper_url = "10.0.0.17"
+$klipper_user = "mks"
 $gcode = @(
+    "RESTART"
     "G28"
-    "G92.3"
-    "G56"
-    "G92 X10 Y10"
+    "G54 G0 X0 Y0"
+    "START_PRINT X=0,100 Y=0,100 Z=0,1"
 )
-
-
-$restart_job = Start-Job {
-    Invoke-RestMethod `
-        -Method Post `
-        -Uri "http://$($using:klipper_url):7125/printer/firmware_restart" `
-        -SkipHttpErrorCheck
-
-    $success = $false
-    while (!$success) {
-        Start-Sleep -Seconds 1
-        $response = Invoke-RestMethod `
-                        -Method Post `
-                        -Uri "http://$($using:klipper_url):7125/printer/gcode/script?script=M117" `
-                        -SkipHttpErrorCheck
-        $success = !($null -ne $response.error)
-    }
-}
 
 git add .
 git commit --amend --no-edit
-git push -f
 
 Write-Host
-Write-Host -ForegroundColor Cyan "== Installing on ${klipper_url} =="
-ssh ${klipper_user}@${klipper_url} "cd ~/mpcnc_post_processor; git fetch; git reset --hard origin/main"
+Write-Host -ForegroundColor Cyan "Installing on ${klipper_url}"
+ssh ${klipper_user}@${klipper_url} "git -C ~/mpcnc_post_processor fetch; git -C ~/mpcnc_post_processor reset --hard origin/main"
 
-
-Write-Host
-Write-Host -ForegroundColor Cyan "== Waiting for klipper to RESTART =="
-Wait-Job -TimeoutSec 10 $restart_job | Out-Null
-Remove-Job $restart_job | Out-Null
 $last_message = (Invoke-RestMethod -Uri http://${klipper_url}:7125/server/gcode_store?count=10).result.gcode_store |
-    Where-Object message -eq M117 |
-    Select-Object -Last 1
+                    Select-Object -Last 1
 
-Write-Host
 $gcode |
     ForEach-Object {
         Write-Host -ForegroundColor Cyan -NoNewline "Running gcode '$_'"
@@ -55,6 +29,13 @@ $gcode |
         Invoke-RestMethod -Method Post -Uri $uri -SkipHttpErrorCheck |
             ForEach-Object {
                 Write-Host -NoNewline ' '
+                if ($uri.EndsWith("=RESTART")) {
+                    5..1 | 
+                        ForEach-Object { 
+                            Write-Host -ForegroundColor Cyan -NoNewline "$_ "
+                            Start-Sleep -Seconds 1
+                        }
+                }
                 if ($null -eq $_.error) {
                     Write-Host -ForegroundColor Green $_.result
                 } elseif ($_.error.message) {
